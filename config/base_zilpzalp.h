9f15f642d07dd43d4e6a255596c80969e429c450
kilipan <kilipan@tutanota.com>
2023-05-17 11:29:29 +0200

    try out urob's timer-less hrm also for layer taps

--- config/base_zilpzalp.h
+++ config/base_zilpzalp.h
@@ -139,7 +139,6 @@
             // opposite side hand keys
             hold-trigger-key-positions = <4 5 6 7 13 14 15 16 17 21 22 23 26 27>;
         };
-
         hr: homerow_mods_right {
             compatible = "zmk,behavior-hold-tap";
             label = "homerow mods right";
@@ -151,6 +150,19 @@
             bindings = <&kp>, <&kp>;
             // opposite side hand keys
             hold-trigger-key-positions = <0 1 2 3 8 9 10 11 12 18 19 20 24 25>;
+        };
+
+        my_lt: my_layer_taps {
+            compatible = "zmk,behavior-hold-tap";
+            label = "my layer taps";
+            #binding-cells = <2>;
+            flavor = "balanced";
+            tapping-term-ms = <170>;
+            quick-tap-ms = <100>;
+            global-quick-tap;
+            bindings = <&mo &kp>, <&kp>;
+            // non-thumb keys
+            hold-trigger-key-positions = <0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23>;
         };
 
         dotcol: dot_colon {
