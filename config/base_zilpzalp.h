c80c731d43f2358736b0fe21d7e8d963050477e8
kilipan <kilipan@tutanota.com>
2023-05-17 10:47:37 +0200

    try fixing overlay again again

--- config/base_zilpzalp.h
+++ config/base_zilpzalp.h
@@ -24,22 +24,22 @@
 /* usually on base layer */
         combo_q {
             timeout-ms = <40>;
-            key-positions = <2 3>;
+            key-positions = <1 2>;
             bindings = <&kp Q>;
         };
         combo_z {
             timeout-ms = <40>;
-            key-positions = <20 21>;
+            key-positions = <18 19>;
             bindings = <&kp Z>;
         };
         combo_v {
             timeout-ms = <40>;
-            key-positions = <1 2>;
+            key-positions = <0 1>;
             bindings = <&kp V>;
         };
         combo_slash {
             timeout-ms = <40>;
-            key-positions = <24 25>;
+            key-positions = <22 23>;
             bindings = <&kp SLASH>;
         };
         combo_minus {
@@ -47,77 +47,77 @@
             key-positions = <21 22>;
             bindings = <&kp MINUS>;
         };
-        combo_enter {
+        combo_esc {
             timeout-ms = <40>;
-            key-positions = <23 24>;
-            bindings = <&kp ENTER>;
+            key-positions = <19 20>;
+            bindings = <&kp ESC>;
         };
 /* parentheticals */
         combo_lbrc {
             timeout-ms = <40>;
-            key-positions = <1 11>;
+            key-positions = <0 9>;
             bindings = <&kp LBRC>;
         };
         combo_lbkt {
             timeout-ms = <40>;
-            key-positions = <2 12>;
+            key-positions = <1 10>;
             bindings = <&kp LBKT>;
         };
         combo_lpar {
             timeout-ms = <40>;
-            key-positions = <3 13>;
+            key-positions = <2 11>;
             bindings = <&kp LPAR>;
         };
         combo_lt {
             timeout-ms = <40>;
-            key-positions = <4 14>;
+            key-positions = <3 12>;
             bindings = <&kp LT>;
         };
         combo_gt {
             timeout-ms = <40>;
-            key-positions = <5 15>;
+            key-positions = <4 13>;
             bindings = <&kp GT>;
         };
         combo_rpar {
             timeout-ms = <40>;
-            key-positions = <6 16>;
+            key-positions = <5 14>;
             bindings = <&kp RPAR>;
         };
         combo_rbkt {
             timeout-ms = <40>;
-            key-positions = <7 17>;
+            key-positions = <6 15>;
             bindings = <&kp RBKT>;
         };
         combo_rbrc {
             timeout-ms = <40>;
-            key-positions = <8 18>;
+            key-positions = <7 16>;
             bindings = <&kp RBRC>;
         };
 /* caps */
         combo_caps-word {
             timeout-ms = <40>;
-            key-positions = <13 16>;
+            key-positions = <11 14>;
             bindings = <&caps_word>;
         };
         combo_capslock {
             timeout-ms = <40>;
-            key-positions = <1 8>;
+            key-positions = <0 7>;
             bindings = <&kp CAPSLOCK>;
         };
 /* deletion */
         combo_bspc {
             timeout-ms = <40>;
-            key-positions = <6 7>;
+            key-positions = <5 6>;
             bindings = <&kp BSPC>;
         };
         combo_del {
             timeout-ms = <40>;
-            key-positions = <7 8>;
+            key-positions = <6 7>;
             bindings = <&kp DEL>;
         };
         combo_delword {
             timeout-ms = <40>;
-            key-positions = <6 8>;
+            key-positions = <5 7>;
             bindings = <&kp LC(BSPC)>;
         };
     };
